f3f1003e84472dd1a956dc03bf5db311948ddb5f
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-08-28 21:01:33 +0000

    Lab 1 & about me done :)

--- lab1/lab1.asm
+++ lab1/lab1.asm
@@ -4,16 +4,56 @@
  ; algorithm: turnin each group of four bits into a digit
  ;            calculate the corresponding ASCII character;
  ;            print the character to the monitor
+;R1 digit counter
+;R2 bit counter
+;R3 is input
+;R4 is for math
+;R5 is also for math bc I'm redundant
+;R0 is the digit
 
  .ORIG x3000
+AND R1,R1, #0; Clear digit counter
+INITD
+ADD R4,R1,#-4
+BRzp DONE
+AND R0,R0, #0; Clear digit
+AND R2,R2, #0; Clear bit counter
+INITDB ADD R5,R2,#-4
+BRzp PBITS ; False
+ADD R0,R0,R0; shift digits left
+ADD R3,R3,#0; to get r3
+BRn ADD1dig ;
+BRnzp SHIFTR3 ;
+ADD1dig 
+ADD R0,R0,#1;
+SHIFTR3
+ADD R3,R3,R3;
+ADD R2,R2,#1
+BRnzp INITDB
+PBITS ADD R5,R0, #-9
+BRp ADDA
+ADD R0,R0, #15
+ADD R0,R0, #15
+ADD R0,R0, #15
+ADD R0,R0, #3; ADD '0'/48
+BRnzp OUTTRAP
+ADDA 
+ADD R0,R0,#15
+ADD R0,R0,#15
+ADD R0,R0,#15
+ADD R0,R0,#10;ADD 'A'-10/55
+OUTTRAP TRAP x21
+ADD R1, R1, #1;
+BRnzp INITD
 
 
 
  ; stop the computer
+DONE
  HALT
 
  ; program data section starts here
-
+;next time make the adding zero thing the jumped branch, kinda unnecessary the way you currently did it
 
  .END
 
