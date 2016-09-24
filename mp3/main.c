a41849a55eb31f1a2e261e2f187d9a7b99084a37
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-09-24 21:05:20 +0000

    MP3

--- mp3/main.c
+++ mp3/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+/* we are given the approximate solution of vout(n+1) which requires knowing vout(n-1) & vout (n), I wrote a for loop that goes n + 1 (0 to N) times, and prints the value of the Voutcurr each time, during each loop i'd make sure to update voutprev to voutcur, and voutcur to voutnew
+*/
 int main()
 {
 	double omega1, omega2, omegac, T, dt;
@@ -16,9 +17,26 @@ int main()
 
 	int i;
 	double Voutnew = 0, Voutcur = 0, Voutprev = 0;
-
+	//printf("Entering Loop\n");
 	//Write your code here!
+
+	for(i=0;i<=N;i++)
+	{//This is the equation given which I broke into parts to increase readability & debugging purposes
+		Voutnew=((1/((sqrt(2))*dt*omegac))-(1/(dt*dt*omegac*omegac)))*Voutprev;
+		Voutnew=Voutnew+(((2/(dt*dt*omegac*omegac)) -1)*Voutcur);
+		Voutnew=Voutnew +sin((omega1*i*dt))+0.5*sin((omega2*i*dt));
+		Voutnew=Voutnew* (1/(((1/(((sqrt(2))*dt*omegac))) +(1/(dt*dt*omegac*omegac))))); //+ parenthesis
+		
+
+	printf("%lf\n",Voutcur); //print out V(n)
+	Voutprev=Voutcur; //update voutprev
+	Voutcur=Voutnew; //update voutcur
+        
+
+
+	}
 
 
 	return 0;
 }
+
