a2d18b40ca08cb2c51db5997153931ae0428770f
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-09-25 21:48:44 +0000

    did the lab4 ikd the spacing

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -3,7 +3,20 @@
 
 int main(){
   // Write the code to take a number n from user and print all the prime numbers between 1 and n
-
+	int number;
+	//int answer;
+	printf("Enter the value of n: ");
+	scanf("%d", &number);
+	//answer= is_prime(number);
+	printf("Printing primes less than or equal to %d:\n" ,number);
+	int i;	
+	for (i=2;i<=number;i++)
+	{
+		if(is_prime(i) ==1)
+		{
+			printf("%d ",i);
+		}
+	}
 
   return 0;
 }
--- lab4/prime.c
+++ lab4/prime.c
@@ -1,7 +1,19 @@
 #include "prime.h"
-
+#include <math.h>
+//int main(){}
 int is_prime(int n){
+int i;
+int answer=1;
   // Return 1 if n is a prime, and 0 if it is not
+ for(i=2;i<n;i++)
+{
+ if((fmod(n,i)==0))
+  {
+	answer=0;
+
+  }
+ }
+return answer;
   return 0;
 }
 
