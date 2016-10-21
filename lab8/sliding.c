b1f8ca09eac1a497bcb54096d1ec775fb03a84e8
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-21 18:48:01 +0000

    lab not done

--- lab8/sliding.c
+++ lab8/sliding.c
@@ -2,6 +2,25 @@
 /*  Slide all values of array up
 */
 void slide_up(int* my_array, int rows, int cols){
-
+int i,j,target_row;
+for (j=0;j<cols;j++){
+	for(i=0;i<rows;i++){
+		if(my_array[i*cols+j]!=0){
+				
+		}
+	}
+}
     return;
 }
+/*
+valgrind invalid read of size 4 bc integer
+gdb
+break sliding.c:16
+run
+p i tells you value of i
+
+next
+display i
+display j
+then next it'll show it
+*/
