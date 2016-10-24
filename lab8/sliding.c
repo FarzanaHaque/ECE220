07562670afba8a2030fccf3948d801984be7c730
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-24 01:45:04 +0000

    hope

--- lab8/sliding.c
+++ lab8/sliding.c
@@ -4,9 +4,15 @@
 void slide_up(int* my_array, int rows, int cols){
 int i,j,target_row;
 for (j=0;j<cols;j++){
-	for(i=0;i<rows;i++){
-		if(my_array[i*cols+j]!=0){
-				
+	for(i=1;i<rows;i++){
+		if(my_array[i*cols+j]!=-1){
+				while(target_row<i&&my_array[target_row*cols+j]!=-1){
+					target_row++;
+					if(target_row<i){
+						my_array[target_row*cols+j]=my_array[i*cols+j];
+						my_array[i*cols+j]=-1;
+					}
+				}
 		}
 	}
 }
