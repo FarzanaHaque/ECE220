c804712e3e44ffc1fd6d6c2fc300fb60d7687415
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-24 02:26:18 +0000

    why didnt it work with ta's code?

--- lab8/sliding.c
+++ lab8/sliding.c
@@ -2,21 +2,25 @@
 /*  Slide all values of array up
 */
 void slide_up(int* my_array, int rows, int cols){
-int i,j,target_row;
+int i,j,target_row=0;
+int a;
 for (j=0;j<cols;j++){
-	for(i=1;i<rows;i++){
+	//target_row=0; //have to make zero again
+	for(i=1;i<rows;i++){//have to start with i=1 bc otherwise won't every have targetrow<i
 		if(my_array[i*cols+j]!=-1){
-				while(target_row<i&&my_array[target_row*cols+j]!=-1){
-					target_row++;
-					if(target_row<i){
-						my_array[target_row*cols+j]=my_array[i*cols+j];
-						my_array[i*cols+j]=-1;
-					}
-				}
+			for(target_row=0;(target_row<i)&&(my_array[target_row*cols+j]!=-1);target_row++){
+			}
+			if(target_row<i){
+				my_array[target_row*cols+j]=my_array[i*cols+j];
+				my_array[i*cols+j]=-1;
+			}
+		
+			
 		}
 	}
 }
     return;
+
 }
 /*
 valgrind invalid read of size 4 bc integer
