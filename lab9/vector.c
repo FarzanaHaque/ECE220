914da44a2501c93ae4d325c346beb4e8b5f5c4c3
fhaque3 <fhaque3@42e793b2-ab26-43ff-96a8-ce2c67eeaf15>
2016-10-31 02:59:32 +0000

    done

--- lab9/vector.c
+++ lab9/vector.c
@@ -4,25 +4,53 @@
 
 vector_t * createVector(int initialSize)
 {
+vector_t * vector=(vector_t*) malloc(sizeof(vector_t));
+vector->size=0;
+vector->maxSize=initialSize;
+vector-> array= (int*) malloc(initialSize*sizeof(int));
+return vector;
 }
 
 void destroyVector(vector_t * vector)
 {
+free(vector->array);
+free(vector);
 }
 
 void resize(vector_t * vector)
 {
+vector->maxSize *= 2;
+vector-> array=(int*) realloc(vector->array,vector->maxSize);
+
 }
 
 void push_back(vector_t * vector, int element)
 {
+vector->size++;
+if(vector->size > vector->maxSize)
+	resize(vector);
+vector->array[vector->size-1]=element;
 }
 
 int pop_back(vector_t * vector)
 {
+if(vector->size <=0)
+{
+return 0;
+}
+else
+{return vector->array[(vector->size)-1];
+//vector->size--;
+//vector-> array=(int*) realloc(vector->array,vector->size -1);
+vector->size--;
+}
 }
 
 int access(vector_t * vector, int index)
 {
+if(index>=vector->size){
+return 0;
+}
+return vector->array [index];
 }
 
